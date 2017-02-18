cd40addf7d68cab20518121e49b757fa33ae4773
pierangelo1982 <pierangelo1982@gmail.com>
2017-02-18 02:31:13 +0100

    esercizio 02

---
+++ 02_numeral-system/02_binary/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("%d - %b \n", 42, 42)
+}
+
+// %d = decimal
+// #b = binary
+// \n è solo a capo
+// quindi il primo 42 diventa decinale e il secondo binario
+// fmt.Printf("%d - %b, decinale, binario")
