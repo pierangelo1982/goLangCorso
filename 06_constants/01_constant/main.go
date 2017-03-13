129081112c2f5aeedd511ecbc175216286e2ca1c
pierangelo1982 <pierangelo1982@gmail.com>
2017-03-13 12:41:06 +0100

    blank identifier constants

---
+++ 06_constants/01_constant/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+const p = "morte e tasse"
+
+func main() {
+	const q = 42
+
+	fmt.Println("p -", p)
+	fmt.Println("q -", q)
+}
+
+// a CONSTANT is a simple unchanging value
+
+// una Costante è un valore incambiambile...
