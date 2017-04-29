b22528f980291c551bc5caf774bd0e4fde367243
pierangelo1982 <pierangelo1982@gmail.com>
2017-04-29 15:38:49 +0200

    20_struct/07_marshal_unmarshal/02_unmarshal/01

---
+++ 20_struct/07_marshal_unmarshal/02_unmarshal/01/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type person struct {
+	First string
+	Last  string
+	Age   int
+}
+
+func main() {
+	var p1 person
+	// i print sotto risulteranno vuoti
+	fmt.Println(p1.First)
+	fmt.Println(p1.Last)
+	fmt.Println(p1.Age)
+
+	bs := []byte(`{"First":"James", "Last":"Bond", "Age":20}`)
+	json.Unmarshal(bs, &p1) // asegno valori a p1
+
+	// qui invece usciranno, perchè passati in bs := byte
+	fmt.Println("------------------")
+	fmt.Println(p1.First)
+	fmt.Println(p1.Last)
+	fmt.Println(p1.Age)
+	fmt.Printf("%T \n", p1)
+}
